de2be5ce1623cc00728e91566b92655a661e392a
Juan Barrios Ferraro <juansebastianferraro@gmail.com>
2023-12-29 14:13:08 -0500

    Added BracketCombinations challenge

--- Challenges/py_challenges.py
+++ Challenges/py_challenges.py
@@ -56,4 +56,32 @@ def caesar_cipher(text):
         i += 1
     return cipher_text
 
-    
+""" Bracket Combinations
+Have the function BracketCombinations(num) 
+read num which will be an integer greater than or equal to zero, 
+and return the number of valid combinations that can be formed with num pairs of parentheses. 
+For example, if the input is 3, then the possible combinations of 3 pairs of parenthesis, 
+namely: ()()(), are ()()(), ()(()), (())(), ((())), and (()()). 
+There are 5 total combinations when the input is 3, so your program should return 5. """
+
+def bracket_combinations(num):
+    total = 0
+
+    def pair_maker(a, c=0):
+        nonlocal total
+
+        if a == 0 and c == 0:
+            total += 1
+
+        if a > 0:
+            pair_maker(a - 1, c + 1)
+
+        if c > 0:
+            pair_maker(a, c - 1)
+
+    pair_maker(num)
+    return total
+
+# Using example
+resultado = bracket_combinations(3)
+print(resultado)

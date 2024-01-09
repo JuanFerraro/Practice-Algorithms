94bdb12543e98330197e176168af52d23334f5a0
Juan Barrios Ferraro <juansebastianferraro@gmail.com>
2024-01-08 20:01:05 -0500

    Added Human Time challenge-> challenges/py_challenges

--- Challenges/py_challenges.py
+++ Challenges/py_challenges.py
@@ -674,3 +674,39 @@ print(find_smallest([ 1, 1, 0, 0, 1, 1],"value"))
 print(find_smallest([ 8, 0, 9], "index"))
 print(find_smallest([ 0], "index"))
 print('************************************************************************************')
+
+""" Challenge 26:
+Human Readable Time
+Write a function, which takes a non-negative integer (seconds) as 
+input and returns the time in a human-readable format (HH:MM:SS)
+HH = hours, padded to 2 digits, range: 00 - 99
+MM = minutes, padded to 2 digits, range: 00 - 59
+SS = seconds, padded to 2 digits, range: 00 - 59 """
+
+def make_readable(seconds):
+    """_Takes # seconds and make it a readable time for humans_
+
+    Args:
+        seconds (_int_): _Seconds quantity_
+
+    Returns:
+        _str_: _Time_
+    """
+
+    hours = seconds // 60 // 60
+    minutes = (seconds - (hours * 60 * 60)) // 60 
+    secs = seconds - ((hours * 60 * 60) + (minutes * 60))
+
+    if hours < 10:
+        hours = "0" + str(hours)
+    if minutes < 10:
+        minutes = "0" + str(minutes)
+    if secs < 10:
+        secs = "0" + str(secs)
+
+    output = str(hours) + ":" + str(minutes) + ":" + str(secs)
+
+    return output
+
+print('Human Time:')
+print(make_readable(86399)) #23:59:59
